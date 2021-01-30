cb2a15125f58f38051d2ebfe03d445394c85c7c4
Adrian Wojciechowicz <adrianwojciechowicz@gmail.com>
2021-01-30 23:13:40 +0100

    MainExtended

---
+++ src/test/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myFavoriteAnimal = "Cat";
+        char myLetter = 'A';
+        int myAge = 35;
+        double myHeight = 1.79;
+        boolean isTrue = true;
+        String noIdea = "Close";
+        System.out.println(myFavoriteAnimal);
+        System.out.println(myLetter);
+        System.out.println(myAge);
+        System.out.println(myHeight);
+        System.out.println(isTrue);
+        System.out.println(noIdea);
+    }
+}
