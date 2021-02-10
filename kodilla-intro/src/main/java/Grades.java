434137c0ef696d9e655d5d2bae0b26a16ad578f4
Adrian Wojciechowicz <adrianwojciechowicz@gmail.com>
2021-02-10 21:29:31 +0100

    New class Grades

---
+++ kodilla-intro/src/main/java/Grades.java
@@ -0,0 +1,31 @@
+public class Grades {
+    private int[] grades;
+    private int size;
+
+    public Grades() {
+        this.grades = new int[10];
+        this.size = 0;
+    }
+
+    public void add(int value) {
+        if (this.size == 10) {
+            return;
+        }
+        this.grades[this.size] = value;
+        this.size++;
+    }
+
+    public int lastNote() {
+        return this.grades[this.size-1];
+    }
+
+    public double averageNote() {
+        int sum = 0;
+        double average = 0;
+        for (int i = 0; i < this.size; i++) {
+            sum = sum + grades[i];
+        }
+        average = (float)sum / (this.size);
+        return average;
+    }
+}
