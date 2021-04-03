c01bae08b610f17b68156e6179452bed9a618059
Adrian Wojciechowicz <adrianwojciechowicz@gmail.com>
2021-04-03 22:09:27 +0200

    Spring - Modul 9.1

---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/SpringBasicRunner.java
@@ -0,0 +1,13 @@
+package com.kodilla.spring.basic;
+
+import com.kodilla.spring.basic.dependency_injection.FacebookMessageService;
+import com.kodilla.spring.basic.dependency_injection.MessageService;
+import com.kodilla.spring.basic.dependency_injection.SimpleApplication;
+
+public class SpringBasicRunner {
+    public static void main(String[] args) {
+        MessageService messageService = new FacebookMessageService();
+        SimpleApplication application = new SimpleApplication(messageService);
+        application.processMessage("Test message", "receiver@mail.com");
+    }
+}
