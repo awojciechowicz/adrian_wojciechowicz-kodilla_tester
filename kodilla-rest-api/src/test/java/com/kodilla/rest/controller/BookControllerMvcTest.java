73dd66304d186fd9a5c55f3736e8797f3fc62665
Adrian Wojciechowicz <adrianwojciechowicz@gmail.com>
2021-04-14 23:00:21 +0200

    Rest API - last commit

--- kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerMvcTest.java
+++ kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerMvcTest.java
@@ -1,10 +1,12 @@
 package com.kodilla.rest.controller;
 
+import com.google.gson.*;
 import com.kodilla.rest.domain.BookDto;
 import com.kodilla.rest.service.BookService;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -12,11 +14,16 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(BookController.class)                   // [1]
@@ -40,7 +47,29 @@ public class BookControllerMvcTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/books")                      // [1]
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().is(200))                     // [2]
-                .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(2)));
+                .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(2)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].title", Matchers.is("title 1")));
+
+    }
+
+    @Test
+    public void shouldAddBooks() throws Exception {
+        //given
+//        List<BookDto> booksList = new ArrayList<>();
+//        booksList.add(new BookDto("title 1", "author 1"));
+//        booksList.add(new BookDto("title 2", "author 2"));
+//        Mockito.when(bookService.getBooks()).thenReturn(booksList);
+
+        //when & then
+        Gson gson = new Gson();
+        BookDto book = new BookDto("title 1", "author 1");
+        String json = gson.toJson(book);
+        System.out.println(json);
+        mockMvc.perform(MockMvcRequestBuilders.post("/books")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(MockMvcResultMatchers.status().is(200));
+        Mockito.verify(bookService,Mockito.times(1)).addBook(book);
     }
 
 }
