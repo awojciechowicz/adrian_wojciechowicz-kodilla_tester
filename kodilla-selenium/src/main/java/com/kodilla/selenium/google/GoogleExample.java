7e5517c8f3803b8c30e18930b28023274ec72d1d
Adrian Wojciechowicz <adrianwojciechowicz@gmail.com>
2021-04-28 23:10:16 +0200

    Selenium Webdriver - Modul 13.3

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/google/GoogleExample.java
@@ -0,0 +1,28 @@
+package com.kodilla.selenium.google;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class GoogleExample {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C://selenium-drivers/Chrome/chromedriver.exe");      // [1]
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+
+        try {
+        Thread.sleep(7000);
+
+        } catch (Exception e) {
+
+        }
+        WebElement inputField = driver.findElement(By.name("q")); // [4]
+        inputField.sendKeys("Kodilla");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("my_Class")));
+        inputField.submit();
+    }
+}
