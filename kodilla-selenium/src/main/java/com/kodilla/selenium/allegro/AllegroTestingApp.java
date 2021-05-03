507f73cb86d30f09f6804db1a3621fdb4092c89a
Adrian Wojciechowicz <adrianwojciechowicz@gmail.com>
2021-05-03 22:43:30 +0200

    XPath - Modul 13.4

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -0,0 +1,37 @@
+package com.kodilla.selenium.allegro;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class AllegroTestingApp {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C://selenium-drivers/Chrome/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://allegro.pl/");
+
+
+//        try {
+//            Thread.sleep(20000);
+//
+//        } catch (Exception e) {
+//
+//        }
+
+//        Alert alert = driver.switchTo().alert();
+//        alert.accept();
+        WebElement category = driver.findElement(
+                By.xpath("//div[contains(@class, \"mp7g_oh mr3m_1 _r65gb\")]/select"));
+
+        Select categorySelect = new Select(category);
+        categorySelect = new Select(category);
+        categorySelect.selectByIndex(3);
+
+        WebElement inputField = driver.findElement(By.name("string"));
+        inputField.sendKeys("Mavic mini");
+        inputField.submit();
+    }
+}
--- kodilla-selenium/src/main/java/com/kodilla/selenium/ebay/EBayTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/ebay/EBayTestingApp.java
@@ -7,10 +7,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class EBayTestingApp {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C://selenium-drivers/Chrome/chromedriver.exe");      // [1]
+        System.setProperty("webdriver.chrome.driver", "C://selenium-drivers/Chrome/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.ebay.com");
 
+        try {
+            Thread.sleep(20000);
+
+        } catch (Exception e) {
+
+        }
         WebElement inputField = driver.findElement(By.name("_nkw"));
         inputField.sendKeys("Laptop");
         inputField.submit();
